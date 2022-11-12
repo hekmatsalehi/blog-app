927f7162079d75c73631962b458161eff996440f
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-11-12 13:28:31 -0800

    Pages structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+]);
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
