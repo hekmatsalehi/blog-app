b354fe45cc6bf6416abda4152a94ac2751ad56fe
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-11-17 14:56:19 -0800

    Register page ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>Register</h1>
+      <form>
+        <input required type='text' placeholder='username'/>
+        <input required type='email' placeholder='email'/>
+        <input required type='password' placeholder='password'/>
+        <p>This is an error</p>
+        <button>Login</button>
+        <span>Do you have an account? <Link to="/login">Sign in</Link></span>
+      </form>
+    </div>
   )
 }
 
