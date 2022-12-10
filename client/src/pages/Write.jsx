7c442f97783797c28df5eb024c71de93ca9dfaae
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-12-10 15:25:10 -0800

    React quill library for post edit

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function Write() {
+  const [value, setValue] = useState('');
   return (
-    <div>Write</div>
+    <div className='add-post'>
+      <div className="content">
+        <input type='text' placeholder='title'/>
+        <div className="editor-container">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">item1</div>
+        <div className="item">item2</div>
+      </div>
+    </div>
   )
 }
 
